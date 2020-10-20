95a4bb48516a2865e59ca3d25ecb33520eec7c62
Simonuis <46494209+Simonuis@users.noreply.github.com>
2020-10-20 18:30:49 +0200

    Image right

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Typical from 'react-typical'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="l">
+          <Typical
+            steps={['Welcome to my portfolio!', 2500, 'My name is Simon Degen', 2500]}
+            loop={Infinity}
+            wrapper="h1"
+          />
+        </div>
+        <div className = "Body">
+          <img className = "Me" src = "Me.jpg" width = "400px" height = "600px"></img>
+        </div>
+      </div>
+      <div className = "banner">
+      </div>
+      <div className = "Nextpage">
+        <p className = "Stickyguys">So, who am i ?</p>
+        <button> Reveal </button>
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Topheader/Topheader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class Topheader extends Component {
+  render() {
+    return(
+      <p>Click on an emoji to view the emoji short name.</p>
+    )
+  }
+}
+export default Topheader; // Don’t forget to use export default!
